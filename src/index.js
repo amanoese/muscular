65c09b9d0184f19faf76cd8974538af3ffd69608
amanoese <amanoese@gmail.com>
2019-08-24 02:34:00 +0900

    implemet shout

--- src/index.js
+++ src/index.js
@@ -3,28 +3,58 @@ const program = require('commander');
 const getStdin = require('get-stdin')
 const FileHound = require('filehound');
 const fs = require('fs');
+const tempy = require('tempy');
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const filesp = FileHound.create()
-  .paths('./data_aa')
+  .paths(`${__dirname}/../data_aa/`)
   .ext('txt')
   .find();
+
+let muscular_shout = () => {
+  let words = fs
+    .readFileSync(`${__dirname}/../data/shout.txt`)
+    .toString()
+    .split('\n')
+  return words[Math.floor(Math.random() * words.length)]
+}
 
 program
 .version(require('../package.json').version)
 .arguments('[text]')
 .action(async text => {
-  let inputText = text || (await getStdin())
-  let files = await filesp
+  let inputText = text ||
+    (await getStdin()) ||
+    muscular_shout();
 
-  let aa_path = files[(Math.random() * files.length).toFixed()]
+  let files = await filesp
+  let index = Math.floor(Math.random() * files.length)
+  let aa_path = files[index]
+
   let raw_aa = fs.readFileSync(aa_path).toString()
   let height = raw_aa.split('\n').length
   let width  = Math.max(...raw_aa.split('\n').map(s=>s.length))
-  let aa = raw_aa.split('\n')
+  let aa_list = raw_aa.split('\n')
     .map(s=>`${s}${' '.repeat(width)}`.slice(0,width))
+
+  let inputTextPath = tempy.file()
+  let regexp = new RegExp(`(.{${height - 2}})`,'g')
+  fs.writeFileSync(inputTextPath,inputText.replace(regexp,'$1\n'))
+
+  let echo_sd_text = (await exec(`${__dirname}/../lib/echo-sd -v $(cat ${inputTextPath})`)).stdout
+  let echo_sd_list = echo_sd_text.split('\n')
+
+  let echo_sd_list_centering = [
+    ...Array.from({length:Math.floor((height - echo_sd_list.length) /2)}),
+    ...echo_sd_list
+  ]
+
+  let output = aa_list
+    .map((v,i)=>v + ' ' + (echo_sd_list_centering[i]||''))
     .join('\n')
 
-  console.log(inputText.replace(/\n$/,''))
-  console.log(aa)
+  console.log(output)
 })
 .parse(process.argv);
