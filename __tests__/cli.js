d1d34a38170f65ad96b5323424185b1f28f7c175
amanoese <amanoese@gmail.com>
2019-08-24 22:17:01 +0900

    fix test

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -7,32 +7,8 @@ const exec = util.promisify(require('child_process').exec);
 describe('muscular',()=>{
   test('pose',async ()=>{
     let { stdout } =await exec(`${appRoot}/src/index-cli.js pose -p フロントリラックス`)
-    expect(stdout.replace('　',' ')).toBe(
-`        .-~-.         
-        /   \\         
-        |   /         
-     ,_-/ ,.*\`--.,    
-   .r ;       \`\`  \\   
-  .\`,\`   ,:  ,.\`A\`,\\  
- /,\`/\\\`''  ''  ? \\\` \\ 
-/  /  \\ ; , ; /   )  }
-| /    \\:':': |   | 7 
-| |    ) ':'  |  ,\` / 
-( \\    \`-,_,-~}  | l  
- \`~   / \`,  /' \\ '"'  
-     ,7   \\/   |.     
-     {  ; |, ,  )     
-     |,\`: |\`,\`: |     
-     \\\` , /\\\`.; /     
-      |\`;/  \\ , |     
-      \\\`\`\\   |\` (     
-      \\ . )  {  /     
-       \\ \`/  (  }     
-        \\ |  | /      
-        ) \\  | |      
-     c~^_~d  V _\`,    
-                      
-`)
+    expect(stdout)
+      .toBe(fs.readFileSync(`${appRoot}/__tests__/pose-result.txt`).toString())
   })
   test('shout',async ()=>{
     let { stdout } =await exec(`${appRoot}/src/index-cli.js shout -p フロントリラックス ナイスバルク`)
