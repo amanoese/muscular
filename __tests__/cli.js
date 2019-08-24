a7b220fead22e19c5ca892e2893d977dedb21e23
amanoese <amanoese@gmail.com>
2019-08-24 21:42:36 +0900

    fix test

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -6,7 +6,7 @@ const exec  = util.promisify(require('child_process').exec);
 describe('muscular',()=>{
   test('pose',async ()=>{
     let { stdout } =await exec(`${appRoot}/src/index-cli.js pose -p フロントリラックス`)
-    expect(stdout).toBe(
+    expect(stdout.replace('　',' ')).toBe(
 `        .-~-.         
         /   \\         
         |   /         
@@ -35,7 +35,7 @@ describe('muscular',()=>{
   })
   test('shout',async ()=>{
     let { stdout } =await exec(`${appRoot}/src/index-cli.js shout -p フロントリラックス ナイスバルク`)
-    expect(stdout).toBe(
+    expect(stdout.replace('　',' ')).toBe(
 `        .-~-.          
         /   \\          
         |   /          
