9982ed8f255982c8659709e586953811b41ec625
amanoese <amanoese@gmail.com>
2019-08-28 02:49:17 +0900

    fix test

--- __tests__/cli.js
+++ __tests__/cli.js
@@ -1,4 +1,5 @@
 const appRoot = `${__dirname}/..`
+const muscular_cmd = `${appRoot}/src/muscular.js`
 
 const fs   = require('fs');
 const util = require('util');
@@ -6,33 +7,33 @@ const exec = util.promisify(require('child_process').exec);
 
 describe('muscular',()=>{
   test('pose',async ()=>{
-    let { stdout } =await exec(`${appRoot}/src/muscular.js pose -p フロントリラックス`)
+    let { stdout } =await exec(`${muscular_cmd} pose -p フロントリラックス`)
     expect(stdout)
       .toBe(fs.readFileSync(`${appRoot}/__tests__/pose-result.txt`).toString())
   })
   test('shout',async ()=>{
-    let { stdout } =await exec(`${appRoot}/src/muscular.js shout -p フロントリラックス ナイスバルク`)
+    let { stdout } =await exec(`${muscular_cmd} shout -p フロントリラックス ナイスバルク`)
     expect(stdout.replace(/　/g,' '))
       .toBe(fs.readFileSync(`${appRoot}/__tests__/shout-result.txt`).toString())
   })
 })
 describe('muscular データの確認',()=>{
   test('pose すべてのポーズが表示できるかの確認',async ()=>{
-    await expect(exec(`muscular pose -l | xargs -L1 muscular pose -p`)).resolves.toBeTruthy()
+    await expect(exec(`${muscular_cmd} pose -l | xargs -L1 ${muscular_cmd} pose -p`)).resolves.toBeTruthy()
   })
   test('shout すべてのポーズが表示できるかの確認',async ()=>{
-    await expect(exec(`muscular shout --pose-list | xargs -L1 muscular shout -p`)).resolves.toBeTruthy()
+    await expect(exec(`${muscular_cmd} shout --pose-list | xargs -L1 ${muscular_cmd} shout -p`)).resolves.toBeTruthy()
   })
 })
 describe('muscular helpコマンドの確認',()=>{
 
   test('help pose',async ()=>{
-    let { stdout } =await exec(`${appRoot}/src/muscular.js help pose`)
+    let { stdout } =await exec(`${muscular_cmd} help pose`)
     expect(stdout).toMatch(/^Usage: muscular pose/)
   })
 
   test('help shout',async ()=>{
-    let { stdout } =await exec(`${appRoot}/src/muscular.js help shout`)
+    let { stdout } =await exec(`${muscular_cmd} help shout`)
     expect(stdout).toMatch(/^Usage: muscular shout \[text\]/)
   })
 
